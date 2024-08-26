1d85b9b1d03fb88a7881d8c8b4d5d20858951aa5
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-08-26 10:47:20 -0300

    modelos de datos

---
+++ src/models/heading.jsx
@@ -0,0 +1,5 @@
+export const heading = {
+    title: "ReactJS", 
+    subtitle:"Desarrollo de Interfaces con JSX",
+    size: 3
+}
---
+++ src/models/links.jsx
@@ -0,0 +1,11 @@
+export const links = [
+  { id: "a12", url: "#home", text: "inicio" },
+  { id: "b57", url: "#gallery", text: "galeria" },
+  { id: "d93", url: "#services", text: "servicios" },
+  { id: "c28", url: "#contact", text: "contacto" },
+]
+export const social = [
+  {id: "", url: "https://facebook.com", text: "facebook"},
+  {id: "", url: "https://instagram.com", text: "instagram"},
+  {id: "", url: "https://api.whatsapp.com/send?phone=1103034567&text=mensaje,por+whatsapp", text: "whatsapp"}
+]
