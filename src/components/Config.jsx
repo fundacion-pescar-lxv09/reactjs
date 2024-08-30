1988f72916fd4f56039d1ac511b74a4d4fe19e1c
Cristian Racedo <cristiandracedo@hotmail.com>
2024-08-30 12:14:42 -0300

    componentes perfil y configuracion

---
+++ src/components/Config.jsx
@@ -0,0 +1,11 @@
+const Config = () => 
+<div>
+    <h2>Opciones de configuracion</h2>
+    <div className="btn-group">
+        <button className="btn btn-primary">editar datos</button>
+        <button className="btn btn-secondary">solicitar informes</button>
+        <button className="btn btn-danger">salir</button>
+    </div>
+</div>
+
+export default Config
---
+++ src/components/Profile.jsx
@@ -0,0 +1,7 @@
+const Profile = () => 
+<div>
+    <h2>Bienvenido a tu Perfil</h2>
+    <p>Realiza alguna accion</p>
+</div>
+    
+export default Profile
