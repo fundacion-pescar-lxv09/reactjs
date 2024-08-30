9f72085421341a0e5632d847d810e2f52fe25e35
Cristian Racedo <cristiandracedo@hotmail.com>
2024-08-30 10:32:14 -0300

    componente de carga

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+const Loading = () => 
+<div className="d-grid justify-content-center align-content-center | py-5">
+    <div className="spinner-border text-dark">
+        <span className="visually-hidden">...Cargando</span>    
+    </div>
+</div>
+
+export default Loading;
