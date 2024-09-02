60037360f9efe01d8a6dfaedb9e1a20a046f6cf6
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-02 10:43:32 -0300

    herencia de metodos a componentes de navegacion

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,20 @@
-function Nav({links}){
+import { NavLink } from 'react-router-dom';
+
+function Nav({links, ...props}){
+    const {fn, host} = props
 return (
     <nav className="navbar navbar-dark navbar-expand-md | p-2 | bg-dark bg-gradient">
-        <a className="navbar-brand" href="#">React</a>
+        <NavLink className="navbar-brand" to="article/posts" onClick={()=>fn(host+"/posts")}>React</NavLink>
         <button className="navbar-toggler navbar-toggler-icon | border-0"></button>
         <ul className="navbar-nav navbar-collapse">
-        { links.map((item,i) => <Link key={i} {...item}/> )}
+        { links.map((item,i) => <Link key={i} {...{...item,...props}}/> )}
         </ul>
     </nav>
 )}
 
-export const Link = ({url,text, target}) => 
-    <li className="list-unstyled"><a className="nav-link" href={url} target={target}>{text}</a></li>
+export const Link = ({url,text, fn, host, type}) => 
+<li className="list-unstyled">
+    <NavLink className="nav-link" to={type+url} onClick={()=>fn(host+url)}>{text}</NavLink>
+</li>
 
 export default Nav;
