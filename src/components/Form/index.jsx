678e4dda2ef7cae242b973520d4a6e73cb7c79a0
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-04 09:55:13 -0300

    crear componente campo de formulario

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,20 +1,19 @@
 const Form = ({fn=console.log, data}) => {
     const handleSubmit = (e) => {
         e.preventDefault()
-        fn && fn(e.target)
+        fn && fn(e)
     }
     return(
     <form onSubmit={handleSubmit}>
         <h2>Completa el Formulario</h2>
-        {
-            Object.keys(data[0]).map(k => 
-            <fieldset className="form-floating m-2">
-                <input className="form-control" type="text" name={k} id={k} placeholder={"Ingrese su "+ k }/>
-                <label htmlFor={k}>{k}</label>
-            </fieldset>)
-        }
+        { Object.keys(data[0]).map((k,i) => <FormControl key={i} /> )}
         <button className="btn btn-success | d-block mx-auto">Enviar</button>
     </form>
 )}
+export const FormControl = ({key, type="text"}) => 
+<fieldset className="form-floating m-2">
+    <input className="form-control" name={key} id={key} placeholder={"Ingrese su "+ key } {...{type}}/>
+    <label htmlFor={key}>{key}</label>
+</fieldset>
 
 export default Form
