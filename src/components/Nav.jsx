be27f26eb6b1ef9b9d2d5efe9c31ac8db6e8258b
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-04 09:54:48 -0300

    implementar contexto en navegacion

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,24 @@
+import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { StateContext } from '../providers/stateContext';
 
-function Nav({links, ...props}){
-    const {fn, host} = props
+function Nav({links}){
+    const { setUrl:fn } = useContext(StateContext)
 return (
     <nav className="navbar navbar-dark navbar-expand-md | p-2 | bg-dark bg-gradient | overflow-auto">
-        <NavLink className="navbar-brand" to="article/posts" onClick={()=>fn(host+"/posts")}>React</NavLink>
+        <NavLink className="navbar-brand" to="article/posts" onClick={()=>fn("posts")}>React</NavLink>
         <button className="navbar-toggler navbar-toggler-icon | border-0"></button>
         <ul className="navbar-nav navbar-collapse">
-        { links.map((item,i) => <Link key={i} {...{...item,...props}}/> )}
+        { links.map((item,i) => <Link key={i} {...item}/> )}
         </ul>
     </nav>
 )}
-
-export const Link = ({url,text, fn, host, type}) => 
-<li className="list-unstyled">
-    <NavLink className="nav-link" to={type+url} onClick={()=>fn(host+url)}>{text}</NavLink>
-</li>
+export const Link = ({url,text,type}) => {
+    const { setUrl:fn } = useContext(StateContext)
+return(
+    <li className="list-unstyled">
+        <NavLink className="nav-link" to={type+"/"+url} onClick={()=>fn(url)}>{text}</NavLink>
+    </li>
+)}
 
 export default Nav;
