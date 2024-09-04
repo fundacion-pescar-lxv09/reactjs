bbe53d11213e28c219a2ab4425679b2ac03a8734
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-04 09:53:31 -0300

    definicion e implementacion de contexto

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { StateProvider } from './providers/stateContext'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <StateProvider>
+      <App />
+    </StateProvider>
   </StrictMode>,
 )
--- src/providers/stateContext.jsx
+++ src/providers/stateContext.jsx
@@ -1,16 +1,22 @@
 import { useState, useEffect, createContext } from "react";
 import { Request } from "../controllers/Request";
+import { jph } from "../models/links";
 export const StateContext = createContext();
 export const StateProvider = ({children}) => {
-    const [state, setState] = useState({ url:'', data: []})
+    const [state, setState] = useState({
+        host: jph, 
+        section:'users', 
+        data: []
+    })
     const set = (key,value) => setState({...state, [key]: value})
-    const setUrl = (url) => set("url", url)
+    const setUrl = (url) => set("section", url)
     const setData = (data) => set("data",data)
     useEffect(() => { 
-        Request(state?.url)
+        const { host, section } = state;
+        Request(host+"/"+section)
         .then(({data}) => setData(data))
         .catch((error) => setData(error))
-    }, [state?.url])
+    }, [state?.section])
     return (
     <StateContext.Provider value={{state,setUrl,setData, set}}>
         {children}
