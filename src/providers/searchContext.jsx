1af43c30e542beac58a337a95900864d7d11ffcf
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-06 09:13:25 -0300

    implementar contexto de busqueda y resultados

---
+++ src/providers/searchContext.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect, createContext, } from "react";
+import axios from "axios";
+import { searchModel } from "../models"
+
+export const SearchContext = createContext()
+export const SearchProvider = ({children}) => {
+    const [search, setSearch] = useState(searchModel)
+    const set = (k,v) => setSearch({...search, [k]:v})
+    useEffect(()=>{
+        const {
+            host:h,
+            resource:r,
+            endpoint:e,
+            api_key:k,
+            q,
+            limit:l,
+            offset:o,
+            rating:rtg,
+            lang:lng,
+            bundle:b,
+        } = search 
+        const url = `${h}/${r}/${e}?api_key=${k}&q=${q}&limit=${l}&offset=${o}&rating=${rtg}&lang=${lng}&bundle=${b}`
+        axios.get(url)
+        .then(res => set("response", res.data))
+    }, [search?.q])
+    return(
+    <SearchContext.Provider value={{search, set}}>
+        {children}
+    </SearchContext.Provider>
+)}
