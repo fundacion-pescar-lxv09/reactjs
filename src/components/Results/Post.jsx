2550cb55ec1407476e9e2242d7b3bb7c34b4f2c8
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-09 10:53:29 -0300

    implementar material-ui en Publicaciones

--- src/components/Results/Post.jsx
+++ src/components/Results/Post.jsx
@@ -1,22 +1,26 @@
 import { useState } from "react"
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Card, CardContent, CardActions, CardMedia } from "@mui/material"
 
-function Post({title,username, import_datetime, images:{ original,downsized_small }}){
+function Post({title,username, import_datetime, images:{ original,downsized_small,fixed_height_small }}){
     const [active, setActive] = useState(false)
     const resize = () => setActive(!active)
-    const [{mp4:oVid,url:oUrl}, {mp4:dsVid,url:dsUrl}] = [original,downsized_small]
+    const [{mp4:oVid,url:oUrl}, {mp4:dsVid,url:dsUrl}, {mp4:sVid,url:sUrl}] = [original,downsized_small, fixed_height_small]
     return(
-    <article className={"post " + active ? "active":""}>
-        <header>
-            <h3>{title ?? "Untitled"}</h3>
-            <p>Publicado por {username ?? "unknown"}</p>
-        </header>
-        <video src={active?(oVid||oUrl):(dsVid||dsUrl)} alt={title} autoPlay={active}/>
-        <p>Publicado el {import_datetime}</p>
-        <footer>
-            <button>Me Gusta</button>
-            <button>Guardar</button>
-            <button onClick={resize}>ver</button>
-        </footer>
-    </article>
+    <Card className={"post " + active ? "active":""} onClick={resize}>
+        <CardMedia component="video" image={active?(oVid||oUrl):(sVid||sUrl||dsVid||dsUrl)} title={title} autoPlay={active} loop={active}/>
+        <CardContent>
+            <Typography variant="h5">{title ?? "Untitled"}</Typography>
+            <Typography variant="body2">Publicado por {username ?? "unknown"}</Typography>
+            <Typography variant="body2">Publicado el {import_datetime}</Typography>
+        </CardContent>
+        <CardActions>
+            <hr/>
+            <Button>Me Gusta</Button>
+            <Button>Guardar</Button>
+            <Button onClick={resize}>ver</Button>
+        </CardActions>
+    </Card>
 )}
 export default Post
--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -2,15 +2,23 @@ import { useContext } from "react";
 import { SearchContext } from "../../providers/searchContext";
 import Post from "./Post";
 import Pagination from "./Pagination";
+import { Grid2 as Grid } from "@mui/material";
 
 function Results(){
     const {search: {q, response}} = useContext(SearchContext)
     const {data, pagination} = response
     return(
-    <section>
-        <h2>Resultados de {q}</h2>
-        { data && data.map(item => <Post key={item.id} {...item}/>)}
+    <Grid container spacing={2} justifyContent="center" sx={{margin: "auto"}}>
+        <Grid size={12}>
+            <h2>Resultados de {q}</h2>
+        </Grid>
+        { data && data.map(item => 
+        <Grid size={{md:6, lg:4, xl:3}}>
+            <Post key={item.id} {...item}/>
+        </Grid> )}
+        <Grid size={12}>
         { pagination ?  <Pagination {...pagination}/> : "No se encontraron resultados" }
-    </section>
+        </Grid>
+    </Grid>
 )}
 export default Results;
