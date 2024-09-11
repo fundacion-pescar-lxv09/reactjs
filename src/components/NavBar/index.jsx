77ae5df81dbd7065a2d7bc7063c33d2753b9262d
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-11 06:32:07 -0300

    implementar componentes de navegacion

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,11 +1,12 @@
 import { navModel } from '../../models'
+import { NavLink } from 'react-router-dom';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material'
 
 function NavBar(){
     return (
     <BottomNavigation sx={{position: "sticky", bottom: 0}} showLabels>
         { navModel.map((props,i) => 
-        <BottomNavigationAction key={i} {...props}/>) }
+        <BottomNavigationAction key={i} {...props} component={NavLink}/> )}
     </BottomNavigation>
 )}
 export default NavBar;
