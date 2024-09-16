0ecd9dd6990587fc601fbe1aa289820e6c77dead
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-16 11:45:04 -0300

    implementar navegacion y enrutamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./controllers/Router"
+import { NavBar } from "./components/NavBar"
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
 return (
-  <>
-
-  </>
+  <BrowserRouter>
+    <NavBar/>
+    <Router/>
+  </BrowserRouter>
 )}
 
 export default App
