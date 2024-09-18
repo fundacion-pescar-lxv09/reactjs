41d3385a00efb8987f946be837b530ed6b1263a9
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-18 11:08:23 -0300

    controlador de solicitud http

---
+++ src/controllers/Request.jsx
@@ -0,0 +1,10 @@
+export default async function Req(url) {
+    try{
+        const response = await fetch(url)
+        const data = await response.json()
+        return data;
+    }
+    catch (error) {
+        return error
+    }
+}
