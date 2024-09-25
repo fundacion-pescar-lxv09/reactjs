808ecc062c480e04d8c2dbc07b5cfb1f9ff780fc
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-25 03:03:09 -0300

    gestionar envio de candidatos seleccionados

--- src/components/Employees/EmployeeList.jsx
+++ src/components/Employees/EmployeeList.jsx
@@ -1,10 +1,24 @@
+import { Button } from "react-bootstrap"
 import {  useSelector } from "react-redux"
 import AreaSelect from "../Areas/AreaSelect"
+import axios from "axios"
+import Req from "../../controllers/Request"
 export default function EmployeeList() {
     const { selected } = useSelector(state => state.employees)
-    console.log(selected)
+    const handleSend = (index) => {
+      const body = selected[index]
+      const host = import.meta.env.VITE_SERVER ?? 'https://localhost:3000/candidates'
+      axios.post(host, body)      
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+    }
   return selected.length && 
     <ul>
-    { selected.map(s => <li>{s.email} <AreaSelect/></li>) }
+    { selected.map((s,i) => 
+      <li key={i}>
+        {s.email} 
+        <AreaSelect/>
+        <Button onClick={ () => handleSend(i)}>Enviar</Button>
+      </li>) }
     </ul>
 }
