fcdebe6ca1ac8baba08936e4173220bf21a3ccb1
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-25 03:04:33 -0300

    instalar y configurar plugin pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa"
+import config from "./manifest.json"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(config)
+  ],
 })
